519b14bbb0564065e0fd1ff61ab05b640a6f36f9
unknown <spacholski99@gmail.com>
2023-06-13 15:14:41 +0200

    Initial empty project in clear architecture infrastructure

---
+++ WebApi/Program.cs
@@ -0,0 +1,30 @@
+using Application;
+using Infrastructure;
+using Presentation;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplication()
+    .AddPresentation()
+    .AddInfrastructure();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
